b2e2ed56eae83e61026906d2f5a0cd4205e3242e
Richard Yantas <richard@dysopsis.com>
2022-05-07 11:21:15 -0500

    task updated

--- week07/task.cpp
+++ week07/task.cpp
@@ -1,7 +1,6 @@
 /*
   Formato de entrega p1.cpp en un zip llamado p1 subir a canvas
 */
-
 #include<iostream>
 #include<vector>
 using namespace std;
@@ -11,7 +10,7 @@ struct point{
 };
 
 bool esta_dentro(vector<point> cuadrado, point punto){
-  // completarr
+  return true;
 };
 
 int main(){
@@ -24,7 +23,44 @@ int main(){
       cin >> cuadrado[i].x >> cuadrado[i].y;
     }
     cin >> punto.x >> punto.y;
-    cout << esta_dentro(cuadrado, punto) << endl;
+    cout << (esta_dentro(cuadrado, punto)?"YES":"NO") << endl; // print YES or NO
   }
   return 0;
 }
+
+/*
+
+5
+0 5
+0 10
+5 5 
+5 10
+-1 1
+-2 2
+2 2
+2 -2
+-2 2
+0 0
+-2 2
+2 2
+2 -2
+-2 2
+2 2
+-2 2
+2 2
+2 -2
+-2 2
+1 1
+2 0
+2 4
+-2 4
+-2 0
+-3 3
+
+NO
+YES
+NO
+YES
+NO
+
+*/
